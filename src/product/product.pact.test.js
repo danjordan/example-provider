d5957eaf375e236da3dfedbd12926c0da84d2565
Dan Jordan <daniel.jordan@mindera.com>
2020-10-22 16:37:12 +0100

    Allow WIP pacts

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -40,7 +40,7 @@ describe("Pact Verification", () => {
       ], // the new way of specifying which pacts to verify
       pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
       enablePending: false,
-      includeWipPactsSince: undefined,
+      includeWipPactsSince: "2020-01-01",
     };
 
     const stateHandlers = {
